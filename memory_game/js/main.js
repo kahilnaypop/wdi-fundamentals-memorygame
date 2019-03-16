c58544cafbda03ce44682afb81544d7b37de4d5e
Kahil Nayton <Kahil@Kahilmacpro.home>
2019-03-16 00:44:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
 console.log("Up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped" + " " + cardOne);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardOne.push ('cardOne');
+
+console.log(cards);
+
+
+var cardTwo = cards[2];
+cardTwo.push ('cardTwo');
+if cardsInPlay === 2 && cardsInPlay[0] = cardsInPlay;
+alert("you found a match!");
+else alert("sorry try again") ;
+
